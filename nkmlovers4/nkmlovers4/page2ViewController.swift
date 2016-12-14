6d82c76d83646a26e88a41db4eedd6ffcfbc8fab
e155745 <e155745@Ibuki-no-MacBook-Pro.local>
2016-12-14 12:47:30 +0900

    commit nkmlovers4

---
+++ nkmlovers4/nkmlovers4/page2ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  page2ViewController.swift
+//  nkmlovers4
+//
+//  Created by Ibuki KAWAMITSU on 2016/12/13.
+//  Copyright © 2016年 Ibuki KAWAMITSU. All rights reserved.
+//
+
+import UIKit
+
+class page2ViewController: UIViewController , UITextFieldDelegate {
+
+    
+    @IBOutlet weak var firstText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.firstText.delegate = self
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
