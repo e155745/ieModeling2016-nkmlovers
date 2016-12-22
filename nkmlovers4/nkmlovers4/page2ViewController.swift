178cbe12bc91577e986e16a812dd675a577b333e
e155745 <e155745@Ibuki-no-MacBook-Pro.local>
2016-12-22 10:39:56 +0900

    modify ver4

--- nkmlovers4/nkmlovers4/page2ViewController.swift
+++ nkmlovers4/nkmlovers4/page2ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class page2ViewController: UIViewController , UITextFieldDelegate {
 
     
+    
     @IBOutlet weak var firstText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
