b9a3737f08a84c7b59eeb15086d86b174375c3cd
e155745 <e155745@Ibuki-no-MacBook-Pro.local>
2017-01-12 16:01:13 +0900

    commit new

---
+++ nkmlovers7/nkmlovers4/ItemTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  ItemTableViewCell.swift
+//  nkmlovers4
+//
+//  Created by Kenta Iwamoto on 2017/01/11.
+//  Copyright © 2017年 Ibuki KAWAMITSU. All rights reserved.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var itemLabel: UILabel!
+  
+    
+    
+    
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
